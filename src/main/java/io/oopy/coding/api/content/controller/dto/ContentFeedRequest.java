208590488a5c4a451151eaad3f222cb6273d4580
jyh0521 <jeonyh0521@gmail.com>
2023-08-07 00:01:19 +0900

    Feat: 게시글 피드 api 틀 작업 및 전체적인 틀 잡음

---
+++ src/main/java/io/oopy/coding/api/content/controller/dto/ContentFeedRequest.java
@@ -0,0 +1,10 @@
+package io.oopy.coding.api.content.controller.dto;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ContentFeedRequest {
+
+}
+
---
+++ src/main/java/io/oopy/coding/common/types/HashTagType.java
@@ -0,0 +1,12 @@
+package io.oopy.coding.common.types;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class HashTagType {
+    private String title;
+}
