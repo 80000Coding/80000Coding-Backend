3a52a7324746d699fddfbc144984623dcb7c5faf
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2023-08-29 00:29:12 +0900

    feat: #13 LegacyEnumValueConverterUtil 정의

--- src/main/java/io/oopy/coding/global/jwt/handler/JwtInterceptor.java
+++
@@ -1,7 +0,0 @@
-package io.oopy.coding.global.jwt.handler;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-
-public class JwtInterceptor implements HandlerInterceptor {
-
-}
--- src/main/java/io/oopy/coding/global/security/UserDetailServiceImpl.java
+++ src/main/java/io/oopy/coding/global/security/UserDetailServiceImpl.java
@@ -1,24 +1,25 @@
 package io.oopy.coding.global.security;
 
 import io.oopy.coding.domain.user.repository.UserRepository;
-import io.oopy.coding.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    @Cacheable(value = "user", key = "#user_id", unless = "#result == null")
-    public UserDetails loadUserByUsername(String user_id) throws UsernameNotFoundException {
-        User user = userRepository.findById(Long.parseLong(user_id))
+    @Cacheable(value = "user", key = "#userId", unless = "#result == null")
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return userRepository.findById(Long.parseLong(userId))
+                .map(CustomUserDetails::of)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-        return CustomUserDetails.of(user);
     }
 }
