60d43938d12edbe66166d572256148a74d856297
codero225 <codero225@gmail.com>
2023-09-15 04:55:46 +0900

    modify user repository

--- src/main/java/io/oopy/coding/domain/user/repository/UserRepository.java
+++ src/main/java/io/oopy/coding/domain/user/repository/UserRepository.java
@@ -2,8 +2,11 @@ package io.oopy.coding.domain.user.repository;
 
 import io.oopy.coding.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByGithubId(Integer githubId);
+    public boolean existsByGithubId(Integer githubId);
+
 }
