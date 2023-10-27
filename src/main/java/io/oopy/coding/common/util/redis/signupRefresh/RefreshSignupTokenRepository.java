6e8e062e827bd01372f46e2a0c51959b99868ece
codero225 <codero225@gmail.com>
2023-10-27 13:28:23 +0900

    Fixed minor issues deleted and modified refresh signup token related files add objectMapper in SecurityFilterConfig for correct initialization of JwtExceptionFilter removed @Content in JwtExceptionFilter modified comparison of role with name to code add oauth info in application.yml

--- src/main/java/io/oopy/coding/common/util/redis/signupRefresh/RefreshSignupTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package io.oopy.coding.common.util.redis.signupRefresh;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshSignupTokenRepository extends CrudRepository<RefreshSignupToken, Integer> {
-
-}
