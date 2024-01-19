a48bebaea264aa054d15d1c3f048019d01b90f5c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-01-19 15:41:25 +0900

    fix: #58 secret key 분리 및 AT Provider 정의

--- src/main/java/io/oopy/coding/common/security/jwt/dto/JwtAuthInfo.java
+++ src/main/java/io/oopy/coding/common/security/jwt/dto/JwtAuthInfo.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 public record JwtAuthInfo(
         Long id,
         RoleType role
-) {
+) implements JwtSubInfo {
     public static JwtAuthInfo of(Long id, RoleType role) {
         return JwtAuthInfo.builder()
                 .id(id)
@@ -21,5 +21,10 @@ public record JwtAuthInfo(
                 .id(user.getId())
                 .role(user.getRole())
                 .build();
+    }
+
+    @Override
+    public String githubId() {
+        throw new UnsupportedOperationException();
     }
 }
